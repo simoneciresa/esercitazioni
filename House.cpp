f426b92013f0045d544067a980c5c56e10364ce6
simoneciresa <simone.ciresa@studenti.unitn.it>
2022-05-11 10:29:24 +0200

    Update main: creazione classe House

---
+++ House.cpp
@@ -0,0 +1,10 @@
+//
+// Created by simone.ciresa on 11/05/22.
+//
+
+#include "House.h"
+#include "room.h"
+
+House::House(int rooms) {
+    Room stanza1(2.5,20,30,4);
+}
---
+++ House.h
@@ -0,0 +1,20 @@
+//
+// Created by simone.ciresa on 11/05/22.
+//
+
+#ifndef ESERCITAZIONI_HOUSE_H
+#define ESERCITAZIONI_HOUSE_H
+
+
+class House {
+private:
+    int rooms;
+public:
+    House(int rooms);
+
+
+
+};
+
+
+#endif //ESERCITAZIONI_HOUSE_H
